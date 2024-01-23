0be52c42cbf08ae5fbb8318b0aac5e62d1d27b1c
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2024-01-23 10:01:35 +0700

    Update FileCode

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const authRoute = require("./routes/auth-route");
+
 const createError = require("./utils/createError")
 const errorHandler = require("./middleware/error");
 const notFoundHandler = require("./middleware/notFound");
 
-
 const web = express();
 web.use(cors());
+
+web.use("/auth", authRoute);
 
 web.use(errorHandler);
 web.use("*", notFoundHandler)
---
+++ src/routes/auth-route.js
@@ -0,0 +1,11 @@
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", () => {});
+router.post("/login", () => {});
+router.post("/forget-password", () => {});
+router.get("/forget-password/:token", () => {});
+router.post("/reset-password", () => {});
+
+module.exports = router;
